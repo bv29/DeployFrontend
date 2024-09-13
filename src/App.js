874199861fa2f9c74b4662bfb5bf691fecd03b0d
bhupesh <bhupeshverma29bv@gmail.com>
2024-09-14 04:08:51 +0530

    first commit 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the website</h1>
+      <SpeedInsights />
+
     </div>
   );
 }
