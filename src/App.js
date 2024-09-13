e7dd79ad3e9c331266b5559070d61dbf2d5683b6
bhupesh <bhupeshverma29bv@gmail.com>
2024-09-14 04:29:36 +0530

    some text edited

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from 'react';
+
 import './App.css';
 import Greeting from './Greeting';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('https://deploy-fx99.onrender.com/')
-      .then((response) => response.text())
-      .then((data) => setMessage(data));
-  }, []);
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <h1>Welcome to the website</h1>
       <Greeting/>
 
     </div>
