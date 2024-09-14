340322eaa2f4b660b17c1621f2babb8180fa6d2d
bhupesh <bhupeshverma29bv@gmail.com>
2024-09-14 17:39:23 +0530

    text edited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
-import Greeting from './Greeting';
+
 
 function App() {
 
   return (
     <div className="App">
       <h1>Welcome to the website</h1>
-      <Greeting/>
+      <h1>Testing deployment </h1>
+
 
     </div>
   );
